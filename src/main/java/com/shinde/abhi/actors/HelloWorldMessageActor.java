9c36fd094dc7bdf3f5d46e72cd9b61c93a7a6f0d
Abhishek <shindeabhi@gmail.com>
2016-07-16 17:21:04 +0530

    AKKA Test Project

--- src/main/java/com/shinde/abhi/actors/HelloWorldMessageActor.java
+++ src/main/java/com/shinde/abhi/actors/HelloWorldMessageActor.java
@@ -1,18 +1,22 @@
 package com.shinde.abhi.actors;
 
 import akka.actor.UntypedActor;
+import akka.event.Logging;
+import akka.event.LoggingAdapter;
 import com.shinde.abhi.pojo.HelloWorldMessage;
 
 /**
  * Created by user on 7/10/2016.
  */
 public class HelloWorldMessageActor extends UntypedActor {
+
+    LoggingAdapter loggingAdapter = Logging.getLogger(getContext().system(),this);
+
     @Override
     public void onReceive(Object message) throws Throwable {
         if (message instanceof HelloWorldMessage) {
+            loggingAdapter.debug(message.toString());
             System.out.print(message);
         }
     }
-
-
 }
